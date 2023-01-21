daf16bdac850d854175d9bf96db54c742b5f8300
MDP87 <hoplalay@mail.ru>
2023-01-21 21:57:54 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.env.Environment;
 
 public class App {
     public static void main(String[] args) {
@@ -7,6 +9,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("compare HelloWorld  = " +  (bean == bean2) );
+        System.out.println("compare Cat = " + (catBean == catBean2));
+
+
     }
 }
